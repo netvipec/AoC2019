4985b6c49ae942b38ddf0fbdd340021d9372a491
Alejandro Rivero <nubcrack@gmail.com>
2020-05-08 15:33:40 +0200

    Solution of Day 9 of AoC 2019.

---
+++ Rust/Day9/part1/src/main.rs
@@ -0,0 +1,205 @@
+use std::io::{self,Write};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn read_input(count : i64) -> i64 {
+    loop {
+        print!("{} - Input: ", count);
+        io::stdout().flush().ok().expect("Could not flush stdout");
+
+        let mut input_text = String::new();
+        io::stdin()
+            .read_line(&mut input_text)
+            .expect("failed to read from stdin");
+
+        let trimmed = input_text.trim();
+        match trimmed.parse::<i64>() {
+            Ok(i) => return i,
+            Err(..) => println!("this was not an integer: {}", trimmed),
+        };
+    }
+}
+
+fn get_parameter(commands: &Vec<i64>, ip : usize, mode : i64, relative_base : i64) -> i64 {
+    match mode {
+        0 => return commands[commands[ip] as usize],
+        1 => return commands[ip],
+        2 => return commands[(relative_base + commands[ip]) as usize],
+        _ => println!("Invalid mode: {}, ip: {}, command: {}, commands: {:?}", mode, ip, commands[ip], commands),
+    }
+    return 0;
+}
+
+fn set_parameter(commands: &mut Vec<i64>, ip : usize, mode : i64, relative_base : i64, value : i64) {
+    match mode {
+        0 => {
+            let input_idx = commands[ip] as usize;
+            commands[input_idx] = value;
+        },
+        1 => println!("Invalid mode: {}, ip: {}, command: {}, commands: {:?}", mode, ip, commands[ip], commands),
+        2 => {
+            let input_idx = (relative_base + commands[ip]) as usize;
+            commands[input_idx] = value;
+        },
+        _ => println!("Invalid mode: {}, ip: {}, command: {}, commands: {:?}", mode, ip, commands[ip], commands),
+    };
+}
+
+fn emulate(commands: &mut Vec<i64>) -> i64 {
+    let mut ip = 0;
+    let mut count = 0;
+    let mut relative_base = 0;
+    loop {
+        if ip >= commands.len() {
+            println!("Outside memory");
+            break;
+        }
+        count += 1;
+        let full_opcode =commands[ip];
+        let opcode = full_opcode % 100;
+        let mode1 = (full_opcode /   100) % 10;
+        let mode2 = (full_opcode /  1000) % 10;
+        let mode3 = (full_opcode / 10000) % 10;
+        match opcode {
+            1 => {
+                if ip + 4 > commands.len() {
+                    println!("Outside memory");
+                    break;
+                }
+                let param1 = get_parameter(commands, ip + 1, mode1, relative_base);
+                let param2 = get_parameter(commands, ip + 2, mode2, relative_base);
+                let new_value = param1 + param2;
+                set_parameter(commands, ip + 3, mode3, relative_base, new_value);
+                ip += 4;
+            },
+            2 => {
+                if ip + 4 > commands.len() {
+                    println!("Outside memory");
+                    break;
+                }
+                let param1 = get_parameter(commands, ip + 1, mode1, relative_base);
+                let param2 = get_parameter(commands, ip + 2, mode2, relative_base);
+                let new_value = param1 * param2;
+                set_parameter(commands, ip + 3, mode3, relative_base, new_value);
+                ip += 4;
+            },
+            3 => {
+                if ip + 2 > commands.len() {
+                    println!("Outside memory");
+                    break;
+                }
+
+                let value = read_input(count);
+                set_parameter(commands, ip + 1, mode1, relative_base, value);
+                ip += 2;
+            },
+            4 => {
+                if ip + 2 > commands.len() {
+                    println!("Outside memory");
+                    break;
+                }
+
+                let value = get_parameter(commands, ip + 1, mode1, relative_base);
+                println!("{} - Output: {}", count, value);
+                ip += 2;
+            },
+            5 => {
+                if ip + 3 > commands.len() {
+                    println!("Outside memory");
+                    break;
+                }
+
+                let param1 = get_parameter(commands, ip + 1, mode1, relative_base);
+                if param1 != 0 {
+                    let value = get_parameter(commands, ip + 2, mode2, relative_base);
+                    if value < 0 {
+                        println!("Invalid jump address {}", value);
+                    }
+                    ip = value as usize;
+                } else {
+                    ip += 3;
+                }
+            },
+            6 => {
+                if ip + 3 > commands.len() {
+                    println!("Outside memory");
+                    break;
+                }
+
+                let param1 = get_parameter(commands, ip + 1, mode1, relative_base);
+                if param1 == 0 {
+                    let value = get_parameter(commands, ip + 2, mode2, relative_base);
+                    if value < 0 {
+                        println!("Invalid jump address {}", value);
+                    }
+                    ip = value as usize;
+                } else {
+                    ip += 3;
+                }
+            },
+            7 => {
+                if ip + 4 > commands.len() {
+                    println!("Outside memory");
+                    break;
+                }
+                let param1 = get_parameter(commands, ip + 1, mode1, relative_base);
+                let param2 = get_parameter(commands, ip + 2, mode2, relative_base);
+                let new_value = if param1 < param2 { 1 } else { 0 };
+                set_parameter(commands, ip + 3, mode3, relative_base, new_value);
+                ip += 4;
+            },
+            8 => {
+                if ip + 4 > commands.len() {
+                    println!("Outside memory");
+                    break;
+                }
+                let param1 = get_parameter(commands, ip + 1, mode1, relative_base);
+                let param2 = get_parameter(commands, ip + 2, mode2, relative_base);
+                let new_value = if param1 == param2 { 1 } else { 0 };                
+                set_parameter(commands, ip + 3, mode3, relative_base, new_value);
+                ip += 4;
+            },
+            9 => {
+                if ip + 2 > commands.len() {
+                    println!("Outside memory");
+                    break;
+                }
+                let param1 = get_parameter(commands, ip + 1, mode1, relative_base);
+                relative_base += param1;
+                ip += 2;
+            },
+
+            99 => {
+                println!("{} - Halting", count);
+                return commands[0];
+            },
+            _ => println!("Invalid command: {}, ip: {}, commands: {:?}", commands[ip], ip, commands),
+        }
+    }
+    return 0;
+}
+
+fn main() {
+    let filename = "src/input";
+    // Open the file in read-only mode (ignoring errors).
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut commands = Vec::new();
+
+    // Read the file line by line using the lines() iterator from std::io::BufRead.
+    for (_, line) in reader.lines().enumerate() {
+        let line = line.unwrap(); // Ignore errors.
+        // Show the line and its number.
+        let commands_str = line.split(",");
+        for comm_str in commands_str {
+            let comm: i64 = comm_str.parse().unwrap();
+            commands.push(comm);
+        }
+        break;
+    }
+
+    commands.resize(2000, 0);
+
+    emulate(&mut commands);
+}
